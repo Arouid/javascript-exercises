334a3f13fa4418dcb7d1e300f905298f1d108ed5
Arouid <scott77598@outlook.com>
2024-07-04 03:09:42 -0500

    add reverse string ()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arr = str.split("");
+    const newArrat = arr.reverse();
+    const joinhello = newArrat.join("")
+    return joinhello;
 };
 
+
+
+
+reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
+""
