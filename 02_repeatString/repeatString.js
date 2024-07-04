f2f381dfe517207c983b1ff432aecbd64eb0fbf3
Arouid <scott77598@outlook.com>
2024-07-04 02:33:46 -0500

    add test 7 code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+    for (let i = 0; i < num; i++) {
+    result += str;
+    }
+    return result;
+}};
 
-};
+function number() {
+    return Math.floor(Math.random() * 1000);
+}
+repeatString('', number());
 
 // Do not edit below this line
 module.exports = repeatString;
