d6f02ee1fd02b0be61a0b18934838f454c1b6c9f
Arouid <scott77598@outlook.com>
2024-07-05 23:52:46 -0500

    add my sumAll answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start, end) {
+    if ((Math.sign(start) === -1) || (Math.sign(end) === -1)) {
+        return 'ERROR';  
+    } else if ((typeof start !== "number") || (typeof end !== "number")) {
+        return 'ERROR';
+    }  
+    if (start > end) {
+    end = [start, start = end][0];
+    }
+    let makeArray = Array.from({length: end - start + 1},(_, index) => start + index);
+    return makeArray.reduce((accu, curr) => accu + curr, 0); 
+}
+sumAll(10,"90");
 
 // Do not edit below this line
 module.exports = sumAll;
