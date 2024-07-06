d03bd779c6b4a619e670535d82849e8d82dff687
Arouid <scott77598@outlook.com>
2024-07-06 01:34:53 -0500

    add leapYears.js answer

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    if (num % 4 === 0 && num % 100 !== 0) {
+        return true;
+    } else if (num % 100 === 0 && num % 400 === 0) {
+        return true;
+    }
+    return false;
+}
 
-};
-
+leapYears(1600)
 // Do not edit below this line
 module.exports = leapYears;
+// if /4 && 400 = true
+// if /4 && 100 = false
+// if /400 && 100 = true
